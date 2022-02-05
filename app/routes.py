f5b5ef5eaff26d539f12d381f091cfaeb1ab5ed8
NIWA HOJI <1>
2022-02-06 01:57:41 +0800

    1

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 from app.models import BookInfo, BookContent, User
+from flask_login import current_user, login_user
 
 
 @app.route("/index")
@@ -31,3 +32,17 @@ def page(book_id, page):
     book_info = BookInfo.query.get(book_id)
     page_count = book_info.page_count
     return render_template("page.html", page_count=page_count, content=content)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
